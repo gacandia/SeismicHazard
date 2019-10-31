b245ac1acbece253e244b20de39d9c07a17d9492
gacandia <45295077+gacandia@users.noreply.github.com>
2019-10-31 08:19:39 -0300

    Bug found in User Defined GMMs
    Bug found in User Defined GMMs - Proble fixed

---
+++ platform_realvalues/pshatoolbox_newGMMfunctions.m
@@ -0,0 +1,15 @@
+clear all
+clc
+
+%% Functions required to include new GMMs
+edit mGMPE_info.m
+edit mGMPEassembleArea.m
+edit mGMPEassembleCrustal.m
+edit mGMPEassembleFault_S.m
+edit mGMPEassembleLine.m
+edit mGMPEassemblePoint.m
+edit mGMPEusp.m
+edit mGMPEVs30.m
+edit mGMPEgetparam.m
+edit mGMPErrupLoop.m
+edit mGMPEdefault.m
